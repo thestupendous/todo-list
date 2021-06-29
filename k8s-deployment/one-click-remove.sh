f6115927052437305a8fcac117ea2775b0b29a2d
Anand Kumar <anand.kumar@gslab.com>
2021-06-29 12:25:20 +0530

    adding namespace, then PV for mongo

--- k8s-deployment/one-click-remove.sh
+++ k8s-deployment/one-click-remove.sh
@@ -4,3 +4,4 @@ kubectl delete deployment front-deployment
 kubectl delete deployment back-deployment
 kubectl delete service front-expose-service
 kubectl delete service back-expose-service
+echo "deleted deployments and services"
