87e74a610c47ad96ec134ae199f6033dc5bcd300
Anand Kumar <anand.kumar@gslab.com>
2021-06-30 19:26:04 +0530

    added pv for storing tasks even after mongo pod dies

--- k8s-deployment/one-click-deploy.sh
+++ k8s-deployment/one-click-deploy.sh
@@ -1,6 +1,8 @@
 #run this file after installing kubernetes
 
 kubectl create -f namespace.yaml
+kubectl create -f pv.yaml
+kubectl create -f pvc.yaml
 kubectl create -f frontend-deployment.yaml
 kubectl create -f backend-deployment.yaml
 kubectl create -f frontend-expose-service.yaml
